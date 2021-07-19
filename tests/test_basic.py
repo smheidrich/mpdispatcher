c545c1d53308392b818d8fad5df3b0e8ea4699a1
smheidrich <smheidrich@weltenfunktion.de>
2021-07-19 14:58:22 +0200

    Add tests for handle_until_closed

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -39,3 +39,48 @@ def test_handle_next_in_parent_proc_with_timeout(dispatcher):
   assert l == [54]
   assert not proc.is_alive()
   assert proc.exitcode == 0
+
+
+def receive_events_until_closed(receiver):
+  l = []
+  def cb(arg):
+    l.append(arg)
+  receiver.connect("cb", cb)
+  receiver.handle_until_closed()
+  assert l == [43, 54, 87]
+
+def test_handle_until_closed_in_child_proc(dispatcher):
+  proc = Process(target=receive_events_until_closed,
+    args=[dispatcher.receiver])
+  proc.daemon = True
+  proc.start()
+  dispatcher.sender.fire("cb", 43)
+  dispatcher.sender.fire("cb", 54)
+  dispatcher.sender.fire("cb", 87)
+  dispatcher.sender.close()
+  proc.join(timeout=2)
+  assert not proc.is_alive()
+  assert proc.exitcode == 0
+
+
+def receive_events_until_87(receiver):
+  l = []
+  def cb(arg):
+    l.append(arg)
+    if arg == 87:
+      receiver.close()
+  receiver.connect("cb", cb)
+  receiver.handle_until_closed()
+  assert l == [43, 54, 87]
+
+def test_handle_until_closed_in_child_proc_closing_itself(dispatcher):
+  proc = Process(target=receive_events_until_87,
+    args=[dispatcher.receiver])
+  proc.daemon = True
+  proc.start()
+  dispatcher.sender.fire("cb", 43)
+  dispatcher.sender.fire("cb", 54)
+  dispatcher.sender.fire("cb", 87)
+  proc.join(timeout=2)
+  assert not proc.is_alive()
+  assert proc.exitcode == 0
