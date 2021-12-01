6eed3b1151fbb0c748211f51f4f1ee786909ac14
smheidrich <smheidrich@weltenfunktion.de>
2021-12-01 21:18:05 +0100

    Fix CI

--- doc/conftest.py
+++ doc/conftest.py
@@ -1,12 +1,12 @@
 from doctest import ELLIPSIS
 from sybil import Sybil
 from sybil.parsers.codeblock import CodeBlockParser
-from sybil.parsers.doctest import DocTestParser, FIX_BYTE_UNICODE_REPR
+from sybil.parsers.doctest import DocTestParser
 from sybil.parsers.skip import skip
 
 pytest_collect_file = Sybil(
     parsers=[
-        DocTestParser(optionflags=ELLIPSIS|FIX_BYTE_UNICODE_REPR),
+        DocTestParser(optionflags=ELLIPSIS),
         CodeBlockParser(),
         skip,
     ],
