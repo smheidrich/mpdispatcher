2dd1bb46698ba8f641f78d3ba6d0fcc10d1eb754
smheidrich <smheidrich@weltenfunktion.de>
2021-12-01 21:21:05 +0100

    Fix CI, part 2 (Sybil)

--- doc/conftest.py
+++ doc/conftest.py
@@ -1,13 +1,13 @@
 from doctest import ELLIPSIS
 from sybil import Sybil
-from sybil.parsers.codeblock import CodeBlockParser
+from sybil.parsers.codeblock import PythonCodeBlockParser
 from sybil.parsers.doctest import DocTestParser
 from sybil.parsers.skip import skip
 
 pytest_collect_file = Sybil(
     parsers=[
         DocTestParser(optionflags=ELLIPSIS),
-        CodeBlockParser(),
+        PythonCodeBlockParser(),
         skip,
     ],
     pattern='*.rst',
